80b5f5470694d0ffb4150bb25e9824f894db9f13
Cuong <cuongngu@gmail.com>
2022-09-20 21:02:06 +0700

    Define routes for managing contacts

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const contactController = require('./controllers/contact.controller');
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to contactbook application'});
-});
+    res.json({ message: 'Welcome to contact book application.' });
+    });
+app.route('/api/contacts')
+    .get(contactController.findAll)
+    .post(contactController.create)
+    .delete(contactController.deleteAll);
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
 
 module.exports = app;
